8540e40fbd782cc295188b4e2f64750f07e355e4
Adeseye <adeseyeogunsina@gmail.com>
2022-05-22 10:30:57 -0700

    added function for printing special characters

---
+++ print_rev.c
@@ -0,0 +1,107 @@
+#include "main.h"
+
+/**
+ * print_bigS - Non printable characters
+ * @ap: va_list of command line arguments to _printf
+ * @f: pointer to the struct of flags
+ * Return: number of char printed
+ */
+int print_bigS(va_list ap, sign_t *f)
+{
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(ap, char *);
+
+	(void)f;
+
+	if (!s)
+		return (_puts("(null)"));
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+
+																			if (!res[1])
+				count += _putchar('0');
+																			count += _puts(res);
+																		}
+		else
+			count += _putchar(s[i]);
+	}
+	return (count);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @ap: command line arguments from _printf
+ * @f: pointer to the struct of flags
+ * Return: length of the printed string
+ */
+int print_rev(va_list ap, sign_t *f)
+{
+	int i = 0, j;
+	char *s = va_arg(ap, char *);
+
+	(void)f;
+
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
+}
+
+/**
+ * print_rot13 - prints a string using rot13
+ * @ap: list of command line arguments from _printf
+ * @f: pointer to the struct of flags
+ * Return: length of the printed string
+ */
+int print_rot13(va_list ap, sign_t *f)
+{
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(ap, char *);
+
+	(void)f;
+
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
+
+	return (j);
+}
+
+/**
+ * print_percent - prints a percent
+ * @ap: list of command_line arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list ap, flags_t *f)
+{
+	(void)ap;
+	(void)f;
+
+	return (_putchar('%'));
+}
